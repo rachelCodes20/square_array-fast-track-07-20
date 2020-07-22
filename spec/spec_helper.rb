7b1be5924fc9f6389db1a99fc0d71c7789c1f9b4
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-22 03:25:59 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 require_relative '../square_array'
 
-
+# your code here
 numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 def  square_array(numbers_list)
-                  # your code here
+                  
   squared_numbers = Array.new
   numbers_list.each do |number|
     squared_numbers << (number * number)
